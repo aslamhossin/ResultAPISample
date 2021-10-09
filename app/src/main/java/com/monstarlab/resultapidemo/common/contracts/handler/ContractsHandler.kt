ddfe655a5d134414bf879f01bf7bee598275b7d9
Atiq <atiqur.rahman@monstar-lab.com>
2021-10-09 11:13:37 +0600

    feat: ContractsHandler implemented

---
+++ app/src/main/java/com/monstarlab/resultapidemo/common/contracts/handler/ContractsHandler.kt
@@ -0,0 +1,34 @@
+package com.monstarlab.resultapidemo.common.contracts.handler
+
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.lifecycle.*
+
+class ContractsHandler<I, O>(
+    private val contract: ActivityResultContract<I, O>,
+    private val registry: ActivityResultRegistry,
+    lifecycleOwner: LifecycleOwner,
+    private val key: String = "activity_rq",
+) : LifecycleObserver {
+
+    private lateinit var resultLauncher: ActivityResultLauncher<I>
+
+    init {
+        lifecycleOwner.lifecycle.addObserver(this)
+    }
+
+    val result: MutableLiveData<O> = MutableLiveData()
+
+    fun launch(input: I) {
+        resultLauncher.launch(input)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun onCreate() {
+        resultLauncher = registry.register(
+            key,
+            contract
+        ) { output -> result.value = output }
+    }
+}
